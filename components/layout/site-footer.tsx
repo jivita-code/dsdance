2635689505dfefbbd992473e09262df0cfb7b55e
Induwara Rasingolla <irasingolla07@gmail.com>
2026-09-12 23:47:07 +0530

    feat: update gallery and footer components for improved layout and accessibility

--- components/layout/site-footer.tsx
+++ components/layout/site-footer.tsx
@@ -24,7 +24,7 @@ export function SiteFooter() {
           </div>
         </div>
       </div>
-      <div data-reveal="text" data-reveal-delay="1"><b>Explore</b><Link href="/who-we-are">Who We Are</Link><Link href="/research-projects">Research Projects</Link><Link href="/news-events">News &amp; Events</Link><Link href="/workshops-gallery">Workshops &amp; Gallery</Link></div>
+      <div data-reveal="text" data-reveal-delay="1"><b>Explore</b><Link href="/who-we-are">Who We Are</Link><Link href="/research-projects">Research Projects</Link><Link href="/news-events">News &amp; Events</Link></div>
       <div data-reveal="text" data-reveal-delay="2"><b>Resources</b><Link href="/blogs">Blogs</Link><Link href="/podcasts">Podcasts</Link><Link href="/contact">Contact</Link><Link href="/join-us">Join Us</Link></div>
       <div data-reveal="text" data-reveal-delay="3"><b>Contact</b><span>London, United Kingdom</span><span>SS17 0BP</span><a href="tel:+447375567669">+44 7375 567669</a><a href="mailto:dsdanceresearchlab@gmail.com">dsdanceresearchlab@gmail.com</a></div>
     </div>
--- config/navigation.ts
+++ config/navigation.ts
@@ -1,7 +1,7 @@
 export const navigation = [
   { href: "/", label: "Home" },
   { href: "/who-we-are", label: "Who We Are", children: [{ href: "/founder", label: "Founder" }, { href: "/advisory-committee", label: "Advisory Committee" }] },
-  { href: "/research-projects", label: "Research Projects", children: [{ href: "/research-projects/past", label: "Past Projects" }, { href: "/research-projects/ongoing", label: "Ongoing Projects" }, { href: "/research-projects/upcoming", label: "Upcoming Projects" }, { href: "/workshops-gallery", label: "Workshops & Gallery" }] },
+  { href: "/research-projects", label: "Research Projects", children: [{ href: "/research-projects/past", label: "Past Projects" }, { href: "/research-projects/ongoing", label: "Ongoing Projects" }, { href: "/research-projects/upcoming", label: "Upcoming Projects" }] },
   { href: "/news-events", label: "News & Events" },
   { href: "/blogs", label: "Resources", children: [{ href: "/blogs", label: "Blogs" }, { href: "/podcasts", label: "Podcasts" }] },
   { href: "/contact", label: "Contact" },
